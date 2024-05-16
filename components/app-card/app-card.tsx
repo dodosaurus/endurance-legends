ee75e17e7846ca8e8e87909df61360a001476fe2
dodosaurus <kovac.jozef25@gmail.com>
2024-05-16 07:16:09 +0200

    better initial design of card

--- components/app-card/app-card.tsx
+++ components/app-card/app-card.tsx
@@ -4,25 +4,27 @@ import Image from "next/image";
 
 function AppCard() {
   return (
-    <Card className="h-96 w-80">
-      <Image src="/images/tadej-pogacar.avif" alt="app card image" width={400} height={330} />
-      <CardHeader>
-        <CardTitle className="flex justify-between items-center gap-2">
-          <span>1</span>
-          <div className="flex justify-end items-center gap-3">
-            <CircleFlag className="w-6 h-6" countryCode="si" />
-            <h2>Tadej Pogačar</h2>
-          </div>
-        </CardTitle>
-        <CardDescription className="flex justify-end items-center">
-          <p>UAE Team Emirates</p>
-        </CardDescription>
-      </CardHeader>
-      {/* <CardContent>
+    <div className="relative h-96 w-80 flex flex-col justify-end border rounded-lg shadow-lg">
+      <Image className="bg-img border rounded-lg" src="/images/tadej-pogacar.avif" alt="app card image" width={400} height={330} />
+      <Card className="h-24">
+        <CardHeader>
+          <CardTitle className="flex justify-between items-center gap-2">
+            <span>1</span>
+            <div className="flex justify-end items-center gap-3">
+              <CircleFlag className="w-6 h-6" countryCode="si" />
+              <h2>Tadej Pogačar</h2>
+            </div>
+          </CardTitle>
+          <CardDescription className="flex justify-end items-center">
+            <p>UAE Team Emirates</p>
+          </CardDescription>
+        </CardHeader>
+        {/* <CardContent>
         <div className="text-xs text-muted-foreground">content goes here</div>
       </CardContent>
       <CardFooter></CardFooter> */}
-    </Card>
+      </Card>
+    </div>
   );
 }
 
