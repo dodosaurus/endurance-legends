768e9c9407aa76becb2a45406404338279da3d05
dodosaurus <kovac.jozef25@gmail.com>
2024-05-25 08:21:55 +0200

    accent color background

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        
+        <div className="bg-[#f8dbb5] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#befff3] absolute top-[-6rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+
         <AppContextProvider>
           <Navbar />
           <main className="flex min-h-screen flex-col items-center justify-between p-1 sm:p-8">{children}</main>
