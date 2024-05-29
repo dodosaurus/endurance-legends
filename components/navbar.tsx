c0ddeee58990526538571d911d36bafedfb33293
dodosaurus <kovac.jozef25@gmail.com>
2024-05-29 07:00:18 +0200

    more dark mode suffering

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,7 +3,7 @@ import ProfileSegment from "./navbar/profile-segment";
 
 const Navbar = () => {
   return (
-    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 dark:bg-black/75 backdrop-blur-lg">
+    <nav className="sticky z-[100] h-14 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/75 dark:bg-slate-950/75 backdrop-blur-lg">
       <div className="flex h-14 items-center justify-between border-b border-gray-200 px-5">
         <Link href="/" className="flex z-40 font-semibold text-2xl">
           endurance<span className="text-cyan-600">vault</span>
