b79c8f38e1afa94757d9bec9b7fe84595158631f
dodosaurus <kovac.jozef25@gmail.com>
2024-05-31 07:46:58 +0200

    dummy landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,31 @@
 import LoginCard from "@/components/login-card";
+import SignupCard from "@/components/signup-card";
+import { Button, buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 // login page only
 
 export default function Home() {
   return (
-    <div id="landingPage">
-      <LoginCard />
+    <div id="landingPage" className="flex flex-col md:flex-row justify-between items-center md:gap-10 gap-2">
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-5xl md:text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Here your fitness meets the <span className="text-cyan-600">real rewards</span>
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-primary">
+          Welcome to Endurance Vault. Train, log Strava activities, earn coins and buy card packs to expand your <span className="font-bold">World Tour cycling</span> collection.
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href="#" className={buttonVariants()}>
+            How it works
+          </Link>
+          <Button variant="ghost">Open source codebase &rarr;</Button>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center gap-5">
+        <SignupCard />
+        <LoginCard />
+      </div>
     </div>
   );
 }
--- components/signup-card.tsx
+++ components/signup-card.tsx
@@ -2,23 +2,21 @@ import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/comp
 import { getStravaLoginUrl } from "@/lib/utils";
 import Link from "next/link";
 import StravaButton from "./login/strava-button";
-import LoaderButton from "./dashboard/panel-cards/subparts/loader-button";
-import { Button } from "./ui/button";
 
-export default function LoginCard() {
+export default function SignupCard() {
   return (
     <Card className="sm:col-span-2">
       <CardHeader className="pb-3">
-        <CardTitle>Login</CardTitle>
+        <CardTitle>Sign Up</CardTitle>
         <CardDescription className="max-w-lg text-balance leading-relaxed">
-          Already part of the community? Log in to enter your dashboard.
+          Sign in with your Strava and let application read your activity and profile data. From the moment of your reigstration your journey will start.
           <br />
           {/* <span className="font-semibold text-cyan-500 pt-2">We need at least permission to read your public activties.</span> */}
         </CardDescription>
       </CardHeader>
       <CardFooter>
         <Link href={getStravaLoginUrl()}>
-          <Button>Enter the Vault</Button>
+          <StravaButton />
         </Link>
       </CardFooter>
     </Card>
