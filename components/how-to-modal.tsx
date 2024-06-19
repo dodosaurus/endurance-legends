348b73967e9db14583ba3192b0654c9f0bd2f633
dodosaurus <kovac.jozef25@gmail.com>
2024-06-19 07:01:28 +0200

    disable how to modal on mobile

--- components/how-to-modal.tsx
+++ components/how-to-modal.tsx
@@ -6,7 +6,7 @@ import { Bike, DoorOpen, LibraryBig, SendToBack } from "lucide-react";
 const HowToModal = ({ triggerVariant = "default" }: { triggerVariant?: "default" | "secondary" }) => {
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger className="hidden sm:block">
         <span className={buttonVariants({ variant: triggerVariant })}>How it works?</span>
       </DialogTrigger>
       <DialogContent className="min-w-96">
