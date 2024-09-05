d64ebcbebc6aca5ee0ea8e532b15596912dc7992
dodosaurus <kovac.jozef25@gmail.com>
2024-09-05 09:06:27 +0200

    display version and build hash

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import packageJson from '../package.json';
 
 export default function Footer() {
   return (
     <footer className="mb-6 text-center text-gray-500 ">
+      <small>Version: {packageJson.version} Build: {process.env.NEXT_PUBLIC_COMMIT_SHA || "local"}</small>
       <small className="mb-1 block text-xs">&copy; {new Date().getFullYear()} by dodosaurus. All rights reserved.</small>
       <small className="block text-xs">
         Powered by{" "}
