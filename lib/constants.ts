75884cd985d299052ef61dd608361c5ed30bdaa7
dodosaurus <kovac.jozef25@gmail.com>
2024-09-20 15:45:16 +0200

    7 days time cap

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const NEW_USER_BONUS_AMOUNT = 300;
-export const SIGNUP_ACTIVITY_TIME_CAP_IN_DAYS = 100;
+export const SIGNUP_ACTIVITY_TIME_CAP_IN_DAYS = 7;
 export const PACK_PRICE = 1000;
 export const PACK_SIZE = 4;
