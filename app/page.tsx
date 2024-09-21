79b4cb0b01b1557db5a97892398d33c1167d67a9
dodosaurus <kovac.jozef25@gmail.com>
2024-09-21 17:29:17 +0200

    getting together gaps on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,24 +4,22 @@ import SignupCard from "@/components/signup-card";
 
 export default function Home() {
   return (
-    <div id="landingPage" className="flex-grow flex flex-col gap-10 w-full">
-      <section className="flex flex-col justify-between items-center md:gap-20 gap-2">
+    <div id="landingPage" className="flex flex-col gap-16 sm:gap-20 md:gap-24 w-full">
+      <section className="flex flex-col items-center gap-8 sm:gap-12 md:gap-16">
         <div className="py-5 mx-auto text-center flex flex-col items-center max-w-3xl">
-          <h1 className="text-5xl font-bold tracking-tight text-primary sm:text-6xl">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold tracking-tight text-primary">
             Here your fitness meets <span className="text-cyan-500">rewards</span>
           </h1>
-          <p className="mt-6 text-xl max-w-prose text-primary">
+          <p className="mt-6 text-lg sm:text-xl max-w-prose text-primary">
             Welcome to Endurance Vault. Train, log Strava activities, earn coins and buy card packs to expand your{" "}
             <span className="font-bold text-amber-500">World Tour cycling</span> collection.
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-5">
-          <SignupCard />
-        </div>
+        <SignupCard />
       </section>
-      <section className="flex flex-col justify-center items-center my-5 gap-10 pt-10">
+      <section className="flex flex-col items-center gap-8 sm:gap-12 md:gap-16">
         <LandingPageHero />
-        <h1 className="max-w-[1000px] text-center text-3xl font-bold tracking-tight text-primary sm:text-4xl md:mt-5">
+        <h1 className="max-w-[1000px] text-center text-2xl sm:text-3xl md:text-4xl font-bold tracking-tight text-primary">
           Unlock your potential by motivating yourself and your friends buy acquiring more common,{" "}
           <span className="text-green-600">uncommon</span>, <span className="text-sky-600">rare</span>,{" "}
           <span className="text-violet-400">epic</span> and <span className="text-amber-600">legendary</span> cards!
